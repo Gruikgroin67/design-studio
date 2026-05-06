8733f8c141f0711dabb4e91e5acb9161c4f53e7d
root <root@jeedom-dev>
2026-05-06 16:47:36 +0200

    fix: correct ajax core include path

--- core/ajax/designstudio.ajax.php
+++ core/ajax/designstudio.ajax.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
     include_file('core', 'authentification', 'php');
 
     if (!isConnect('admin')) {
